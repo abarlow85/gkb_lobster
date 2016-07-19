c195e5ebb992214a500727a8ca0943686e41976a
Alec Barlow <abarlow85@gmail.com>
2016-07-19 12:56:00 -0700

    api errors for components

--- apps/bike_donations/views.py
+++ apps/bike_donations/views.py
@@ -136,12 +136,15 @@ def component_post(request):
 
 	if form.is_valid():
 		lightspeed = LightspeedApi()
-		newComponent = lightspeed.create_item(descriptionString, int(parsed_json['price']))
-		request.session['customSku'] = newComponent['customSku']
-		request.session['price'] = parsed_json['price']
-		request.session['type'] = itemType
+		newComponent = lightspeed.create_item(descriptionString, int(parsed_json['price']), request.user.username)
 
-		return JsonResponse({'success' : True})
+		if newComponent['status'] == 200:
+			request.session['customSku'] = newComponent['customSku']
+			request.session['price'] = parsed_json['price']
+			request.session['type'] = itemType
+			return JsonResponse({'status' : True})
+		else:
+			return JsonResponse({'status' : False, 'error' : newComponent['status']})
 
 	else:
 		print ("Not valid", form.errors.as_json())
