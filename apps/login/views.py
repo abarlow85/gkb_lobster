3a4b94edd7842556cf83c96650bddc57bab9610d
dancinturtle <amy.giver@gmail.com>
2016-07-22 12:40:20 -0700

    debugged back to login issues

--- apps/login/views.py
+++ apps/login/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 
 # Create your views here.
 def home(request):
     return render(request, 'login/index.html')
 
 def verifyUser(request):
+    print(request.user);
     if request.method == "POST":
         user = authenticate(username = request.POST["userType"], password = request.POST["password"])
         if user is not None:
@@ -21,3 +22,10 @@ def verifyUser(request):
         else:
             print("doesn't exist")
             return render(request, 'login/index.html', {"error":"Invalid password for " + request.POST["userType"]})
+    else:
+        return HttpResponseRedirect('/login')
+
+def csrf_failure(request, reason="Stop it"):
+    print "csrf time"
+    logout(request)
+    return render(request, 'login/index.html', {"error":"We had to log out the previous user. Please try again."})
