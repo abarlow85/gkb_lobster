7d73fb7af4aceb9ac3dc9c71173dbeef5fb5346a
Alec Barlow <abarlow85@gmail.com>
2016-07-26 14:34:56 -0700

    proc again

--- gkb_lobster/wsgi.py
+++ gkb_lobster/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gkb_squid.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gkb_lobster.settings")
 
 application = get_wsgi_application()
