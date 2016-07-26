bba55c423e045d94b737003a59a9d125eee7a334
Alec Barlow <abarlow85@gmail.com>
2016-07-26 15:01:55 -0700

    static

--- gkb_lobster/settings.py
+++ gkb_lobster/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -136,12 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
 STATICFILES_DIRS = [
-
+    
 ]
 
 LOGIN_URL = '/login'
