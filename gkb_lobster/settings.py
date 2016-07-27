8d7e8ce24bc789f7779c8f015cee93daee8bcbd9
Alec Barlow <abarlow85@gmail.com>
2016-07-27 10:17:29 -0700

    csrf

--- gkb_lobster/settings.py
+++ gkb_lobster/settings.py
@@ -103,7 +103,7 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_HTTPONLY = True
+# CSRF_COOKIE_HTTPONLY = True
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
